909593b298c7dee1fde0a923ebcd61b1b35fc45c
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-02-01 16:47:01 -0300

    los usuarios se guardan como lista

---
+++ tortuga.py
@@ -0,0 +1,19 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100) # Avanza 100 pixeles en la dirección que está
+tortuga.right(90) # Gira 90 grados a la derecha
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+
+window.mainloop()
